4ef49437b24ddf228f00de42b2d8289d31ba0338
David Hardy <davehardy632@gmail.com>
2019-08-13 17:36:54 -0600

    Created initial test to ensure the proper connection and response is being returned from the root of the application.

--- app.spec.js
+++ app.spec.js
@@ -1,3 +1,24 @@
 var shell = require("shelljs")
 var request = require("supertest")
 var app = require("./app")
+
+describe("api", () => {
+  beforeAll(() => {
+    shell.exec('npx sequelize db:create')
+  });
+  beforeEach(() => {
+    shell.exec('npx sequelize db:migrate')
+    shell.exec('npx sequelize db:seed:all')
+  });
+  afterEach(() => {
+    shell.exec('npx sequelize db:migrate:undo:all')
+  });
+
+  describe ("test the root path", () => {
+    test("should return a 202 status", () => {
+      return request(app).get("/").then(response => {
+        expect(response.statusCode).toBe(200);
+      })
+    })
+  })
+})
