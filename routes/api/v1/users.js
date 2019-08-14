d46ab49653f671fb7f678127a23ad62001cd0ab0
David Hardy <davehardy632@gmail.com>
2019-08-13 19:30:06 -0600

    Created a route for users to be able to create an account and recieve an api key.

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -2,8 +2,21 @@ var express = require("express");
 var router = express.Router();
 var User = require('../../../models').User;
 
-// router.post("/games", function(req, res, next) {
-//   User.create({
-//
-//   })
-// })
+router.post("/", function(req, res, next) {
+  
+  User.create({
+          email: req.body.email,
+          password: req.body.password,
+          password_confirmation: req.body.password_confirmation
+    })
+    .then(user => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify(user));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+});
+
+module.exports = router;
