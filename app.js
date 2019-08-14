24d04a77e5f55dfcf74caea76427e890fd9abc03
David Hardy <davehardy632@gmail.com>
2019-08-14 16:56:19 -0600

    Created a sessions route for a user to send a post request with their account information, email and password, if they are registered in the system, their api key is returned to them in the response body.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/api/v1/users');
+var sessionsRouter = require('./routes/api/v1/sessions');
 // add on line 18
 
 
@@ -17,6 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/sessions', sessionsRouter)
 
 
 module.exports = app;
