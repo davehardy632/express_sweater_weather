e525fcdd1a8db4c2bd4c7f239aa605beec60505f
David Hardy <davehardy632@gmail.com>
2019-08-14 14:13:32 -0600

    Installed uuid-apikey to generate an api key when a user is created via post, also a user is only created when their password and password confirmation match.

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,25 +3,30 @@ var router = express.Router();
 var User = require('../../../models').User;
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const uuidAPIKey = require('uuid-apikey');
 
 router.post("/", function(req, res, next) {
   let hash = bcrypt.hashSync(req.body.password, saltRounds);
-  // if bcrypt.compareSync(req.body.password_confirmation, hash);
-  console.log(bcrypt.compareSync(req.body.password_confirmation, hash));
-
-  User.create({
-          email: req.body.email,
-          password: hash,
-          apiKey: "12345"
-    })
-    .then(game => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(201).send(JSON.stringify(game));
-    })
-    .catch(error => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(500).send({ error });
-    });
+  let newKey = uuidAPIKey.create()["apiKey"];
+  if (bcrypt.compareSync(req.body.password_confirmation, hash)) {
+      User.create({
+        email: req.body.email,
+        password: hash,
+        apiKey: newKey
+      })
+      .then(user => {
+        apiKeyResponse = {"api_key": user["apiKey"]}
+        res.setHeader("Content-Type", "application/json");
+        res.status(201).send(JSON.stringify(apiKeyResponse));
+      })
+      .catch(error => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(500).send({ error });
+      });
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(400).send(JSON.stringify("Password confirmation does not match"));
+  }
 });
 
 module.exports = router;
