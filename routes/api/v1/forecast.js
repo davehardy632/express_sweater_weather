ca7297125f053c1d4cb4758ca8adf51163813daf
David Hardy <davehardy632@gmail.com>
2019-08-15 18:42:56 -0600

    Successfully made an api call to google geocode to retrieve latitude and longitude from a passed in location parameter.

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,25 +1,50 @@
 var express = require("express");
 var router = express.Router();
 var User = require('../../../models').User;
+const fetch = require("node-fetch");
+// var latLongService = require('../../../services/GoogleGeocodingApiService');
 
 router.get("/", function(req, res, next) {
   let locationValue = req.url.split("=")[1];
-  let keyParam = req.url.split("=")[0].substr(2);
-  let stringArgument = keyParam + " " + locationValue
-  User.findAll({
-    where: {
-      apiKey: req.body.api_key
+  // var latLong = latLongService.returnLocation(locationValue)
+  if (User.findAll({ where: { apiKey: req.body.api_key }
+    }).then(user => { return true })
+    .catch(error => console.error({ error }))) {
+      fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCa4n7lyqXASgTqCmCcV6EbTUhWM65tgZo&address=${locationValue}`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(JSON.stringify(myJson));
+      })
+      .catch(error => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(500).send({ error });
+      });
+
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.status(400).send(JSON.stringify("Invalid request parameters."));
     }
-  })
-  .then(user => {
-    
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).send(JSON.stringify(user));
-  })
-  .catch(error => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(500).send({error})
-  });
+
 });
 
 module.exports = router;
+
+// fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCa4n7lyqXASgTqCmCcV6EbTUhWM65tgZo&address=${locationValue}`)
+// .then(response => {
+//   console.log(keyParam);
+//   res.setHeader("Content-Type", "application/json");
+//   res.status(200).send(response.json());
+//   // res.status(200).send(JSON.stringify(response.body));
+// })
+// .catch(error => console.error({ error }));
+// }
+
+
+// res.setHeader("Content-Type", "application/json");
+// res.status(200).send(JSON.stringify(latLong));
+// res.setHeader("Content-Type", "application/json");
+// res.status(500).send({error})
--- services/googleGeocodingApiService.js
+++ services/googleGeocodingApiService.js
@@ -1,12 +1,24 @@
-class googleGeocodingApiService {
-    constructor(location) {
-        this.location = location;
-    }
+const fetch = require("node-fetch");
 
-    returnLatLong() {
-      fetch(`https://hedgehog-party.herokuapp.com/api/v1/invites`)
-      .then(response => response.json())
-      .then(hedgehogs => appendHedgehogs(hedgehogs))
-      .catch(error => console.error({ error }));
-    }
+class GoogleGeocodingApiService {
+  constructor() {
+      this.latLong = {};
+  }
+
+  returnLocation (location) {
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCa4n7lyqXASgTqCmCcV6EbTUhWM65tgZo&address=${location}`)
+    .then(response => response.json())
+    .catch(error => console.error({ error }));
+  }
 }
+
+
+
+var latLongService = new GoogleGeocodingApiService();
+
+module.exports = latLongService;
+
+
+// .then(response => response.json())
+// .then(hedgehogs => appendHedgehogs(hedgehogs))
+// .catch(error => console.error({ error }));
