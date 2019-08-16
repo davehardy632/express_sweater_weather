9eb29106824bd4ae9078b98dd8bcf0f95b30caaa
David Hardy <davehardy632@gmail.com>
2019-08-16 16:06:31 -0600

    Completed user story 3, when a user passes a valid api key and location, the google geocode api service is hit, then the latitude and longitude are returned and passed into a new api call that returns the current daily and hourly forecast.

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -6,9 +6,8 @@ const fetch = require("node-fetch");
 
 router.get("/", function(req, res, next) {
   let locationValue = req.url.split("=")[1];
-  if (User.findAll({ where: { apiKey: req.body.api_key }
-    }).then(user => { return true })
-    .catch(error => console.error({ error }))) {
+    User.findAll({ where: { apiKey: req.body.api_key }})
+    .then(function(user) {
       fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCa4n7lyqXASgTqCmCcV6EbTUhWM65tgZo&address=${locationValue}`)
       .then(function(response) {
         return response.json();
@@ -42,17 +41,69 @@ router.get("/", function(req, res, next) {
           res.setHeader("Content-Type", "application/json");
           res.status(500).send({ error });
         });
+        })
+        .catch(error => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(500).send({ error });
+        });
       })
-      .catch(error => {
-        res.setHeader("Content-Type", "application/json");
-        res.status(500).send({ error });
-      });
-
-    } else {
+    .catch(error => {
       res.setHeader("Content-Type", "application/json");
-      res.status(400).send(JSON.stringify("Invalid request parameters."));
-    }
-});
+      res.status(500).send({ error });
+  });
+})
+
+
+
+// router.get("/", function(req, res, next) {
+//   let locationValue = req.url.split("=")[1];
+//   if (User.findAll({ where: { apiKey: req.body.api_key }})
+//     .then(user => { return true })
+//     .catch(error => console.error({ error }))) {
+//       fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCa4n7lyqXASgTqCmCcV6EbTUhWM65tgZo&address=${locationValue}`)
+//       .then(function(response) {
+//         return response.json();
+//       })
+//       .then(function(myJson) {
+//         return myJson["results"][0]["geometry"]["location"];
+//       })
+//       .then(function(latLongObject) {
+//         latitude = latLongObject["lat"];
+//         longitude = latLongObject["lng"];
+//         fetch(`https://api.darksky.net/forecast/a0afd4046e86e2555d0a937cdac811fe/39.7392358,-104.990251?latitude=${latitude}&longitude=-${longitude}&exclude=minutely,alerts,flags`)
+//         .then(function(response) {
+//           return response.json();
+//         })
+//         .then(function(myJson) {
+//           forecastObj = {};
+//
+//           forecastObj["location"] = locationValue;
+//           forecastObj[Object.keys(myJson)[3]] = myJson["currently"];
+//
+//           forecastObj["hourly"] = myJson["hourly"];
+//           forecastObj["hourly"]["data"] = myJson["hourly"]["data"].slice(0, 8);
+//
+//           forecastObj["daily"] = myJson["daily"]
+//           forecastObj["daily"]["data"] = myJson["daily"]["data"].slice(0, 7)
+//
+//           res.setHeader("Content-Type", "application/json");
+//           res.status(200).send(JSON.stringify(forecastObj));
+//         })
+//         .catch(error => {
+//           res.setHeader("Content-Type", "application/json");
+//           res.status(500).send({ error });
+//         });
+//       })
+//       .catch(error => {
+//         res.setHeader("Content-Type", "application/json");
+//         res.status(500).send({ error });
+//       });
+//
+//     } else {
+//       res.setHeader("Content-Type", "application/json");
+//       res.status(400).send(JSON.stringify("Invalid request parameters."));
+//     }
+// });
 
 module.exports = router;
 
--- services/googleGeocodingApiService.js
+++ services/googleGeocodingApiService.js
@@ -1,22 +1,99 @@
-const fetch = require("node-fetch");
-
-class GoogleGeocodingApiService {
-  constructor() {
-      this.latLong = {};
-  }
-
-  returnLocation (location) {
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCa4n7lyqXASgTqCmCcV6EbTUhWM65tgZo&address=${location}`)
-    .then(response => response.json())
-    .catch(error => console.error({ error }));
-  }
-}
 
 
 
-var latLongService = new GoogleGeocodingApiService();
 
-module.exports = latLongService;
+
+
+
+
+router.get("/", function(req, res, next) {
+  let locationValue = req.url.split("=")[1];
+  if (User.findAll({ where: { apiKey: req.body.api_key }})
+    .then(user => { return true })
+    .catch(error => console.error({ error }))) {
+      fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCa4n7lyqXASgTqCmCcV6EbTUhWM65tgZo&address=${locationValue}`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        return myJson["results"][0]["geometry"]["location"];
+      })
+      .then(function(latLongObject) {
+        latitude = latLongObject["lat"];
+        longitude = latLongObject["lng"];
+        fetch(`https://api.darksky.net/forecast/a0afd4046e86e2555d0a937cdac811fe/39.7392358,-104.990251?latitude=${latitude}&longitude=-${longitude}&exclude=minutely,alerts,flags`)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          forecastObj = {};
+
+          forecastObj["location"] = locationValue;
+          forecastObj[Object.keys(myJson)[3]] = myJson["currently"];
+
+          forecastObj["hourly"] = myJson["hourly"];
+          forecastObj["hourly"]["data"] = myJson["hourly"]["data"].slice(0, 8);
+
+          forecastObj["daily"] = myJson["daily"]
+          forecastObj["daily"]["data"] = myJson["daily"]["data"].slice(0, 7)
+
+          res.setHeader("Content-Type", "application/json");
+          res.status(200).send(JSON.stringify(forecastObj));
+        })
+        .catch(error => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(500).send({ error });
+        });
+      })
+      .catch(error => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(500).send({ error });
+      });
+
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.status(400).send(JSON.stringify("Invalid request parameters."));
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fetch = require("node-fetch");
+//
+// class GoogleGeocodingApiService {
+//   constructor() {
+//       this.latLong = {};
+//   }
+//
+//   returnLocation (location) {
+//     fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCa4n7lyqXASgTqCmCcV6EbTUhWM65tgZo&address=${location}`)
+//     .then(response => response.json())
+//     .catch(error => console.error({ error }));
+//   }
+// }
+//
+//
+//
+// var latLongService = new GoogleGeocodingApiService();
+//
+// module.exports = latLongService;
 
 // "https://api.darksky.net/forecast/a0afd4046e86e2555d0a937cdac811fe/39.7392358,-104.990251?latitude=39.7392358&longitude=-104.990251&exclude=minutely,alerts,flags"
 
