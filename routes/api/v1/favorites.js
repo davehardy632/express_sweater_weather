e2416a0a019e27f1699f6fd6ad37261939d2a8b6
David Hardy <davehardy632@gmail.com>
2019-08-16 19:14:44 -0600

    Started router method to return current forecasts from each of the users stored favorite locations.

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -30,4 +30,23 @@ router.post("/", function(req, res, next) {
   });
 })
 
+router.get("/", function(req, res, next) {
+  key = req.body.api_key
+  User.findAll({ where: { apiKey: key }})
+  .then(function(user) {
+    id = user[0]["dataValues"]["id"];
+    Location.findAll({ where: { UserId: id }})
+    .then(function(locations) {
+      locationArray = [];
+      for (const key of Object.keys(locations)) {
+        locationArray.push(locations[key]["dataValues"]["name"]);
+      }
+      return locationArray;
+    })
+    .then(function(favorites) {
+      favorites
+    })
+  })
+})
+
 module.exports = router;
