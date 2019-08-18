a1b0dc548f6b116ea1be641228f37349cf059264
David Hardy <davehardy632@gmail.com>
2019-08-18 13:08:17 -0600

    Completed user story 5, a user can get the current forecast from each of their previously saved locations.

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -51,21 +51,6 @@ function arrayOfFavoriteLocations(key) {
   });
 }
 
-/////// return latitude and longitude
-
-// function returnLatLong(element) {
-//   fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCa4n7lyqXASgTqCmCcV6EbTUhWM65tgZo&address=${element}`)
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(myJson) {
-//     console.log(myJson["results"][0]["geometry"]["location"]);
-//   })
-//   .catch(error => {
-//     res.setHeader("Content-Type", "application/json");
-//     res.status(500).send({ error });
-//   });
-// }
 
 let fetchDataFromApi = async (location) => {
   let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCa4n7lyqXASgTqCmCcV6EbTUhWM65tgZo&address=${location}`);
@@ -73,16 +58,28 @@ let fetchDataFromApi = async (location) => {
   return results["results"][0]["geometry"]["location"];
 }
 
+let fetchForecastFromApi = async (coordinateObj) => {
+  let response = await fetch(`https://api.darksky.net/forecast/a0afd4046e86e2555d0a937cdac811fe/${coordinateObj["lat"]},${coordinateObj["lng"]}?exclude==minutely,hourly,daily,alerts,flags`);
+  let results = await response.json();
+  return results;
+}
+
 let favoriteLocations = async (key) => {
   let response = await User.findAll({ where: { apiKey: key }})
   let userId = response[0]["dataValues"]["id"]
-  let locations = Location.findAll({ where: { UserId: userId }})
-  let allLocations = []
+  let locations = await Location.findAll({ where: { UserId: userId }}).then(returnValue => {
+    return returnValue
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({ error });
+  });
+  // let allLocations = []
   return locations;
 }
 
 let accrueLatLong = async (array) => {
-  let coordinates = [];
+  let coordinates= [];
   for (let i = 0; i < array.length; i++) {
     let res = await fetchDataFromApi(array[i]);
     coordinates.push(res);
@@ -90,104 +87,35 @@ let accrueLatLong = async (array) => {
   return coordinates;
 }
 
+let accrueForecasts = async (array) => {
+  let forecastArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let res = await fetchForecastFromApi(array[i]);
+    forecastArray.push(res);
+  }
+  return forecastArray;
+}
+
 
 router.get("/", async function(req, res, next) {
   userKey = req.body.api_key
 
-  favorites = await favoriteLocations(userKey)
-  // latitudeAndLongitude = await fetchDataFromApi("Denver, CO")
+  if (userKey == undefined) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(400).send(JSON.stringify("Api key invalid."));
+  } else {
+    favorites = await favoriteLocations(userKey)
+    favoriteArray = [];
+    favorites.forEach(function(element) {
+      favoriteArray.push(element["dataValues"]["name"]);
+    })
 
-  favoriteArray = [];
+    latLongArray = await accrueLatLong(favoriteArray)
+    favoriteForecasts = await accrueForecasts(latLongArray)
 
-  favorites.forEach(function(element) {
-    favoriteArray.push(element["dataValues"]["name"]);
-  })
-
-  latLongArray = await accrueLatLong(favoriteArray)
-
-  console.log(latLongArray[0]["lat"]);
-
-  // User.findAll({ where: { apiKey: key }})
-  // .then(function(user) {
-  //   id = user[0]["dataValues"]["id"];
-  //   Location.findAll({ where: { UserId: id }})
-  //   .then(function(locations) {
-  //     locationArray = [];
-  //     for (const key of Object.keys(locations)) {
-  //       locationArray.push(locations[key]["dataValues"]["name"]);
-  //     }
-  //     return locationArray;
-  //   })
-  //
-  //
-  //   .then(function(favorites) {
-  //     coordinateArray = [];
-  //
-  //     favorites.forEach(function(element) {
-  //     coordinateArray.push(fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCa4n7lyqXASgTqCmCcV6EbTUhWM65tgZo&address=${element}`)
-  //       .then(function(response) {
-  //         return response.json();
-  //       })
-  //       .then(function(myJson) {
-  //         element = myJson["results"][0]["geometry"]["location"]
-  //         return element;
-  //       })
-  //       // above is the end of the first then statement
-  //       .catch(error => {
-  //         res.setHeader("Content-Type", "application/json");
-  //         res.status(500).send({ error });
-  //       })
-  //       // above is supposedly the end of the catch statement for the fetch async function
-  //
-  //       // above is semicolon for the return value in the forEach block
-  //       )
-  //       // above is the end of the puush function to the end of the array
-  //     });
-  //
-  //     return coordinateArray;
-  //     // above is ending to array iteration function
-  //   })
-  //
-  //
-  //   .then(async function(myJson) {
-  //
-  //     // const infoArray = [];
-  //     // console.log(myJson)
-  //     promise1 = myJson[0]
-  //     promise2 = myJson[1]
-  //
-  //     promise1.then(function(value) {
-  //       value;
-  //     });
-  //
-  //     const result = await promise1
-  //     const result2 = await promise2
-  //
-  //     console.log(result)
-  //     console.log(result2)
-  //
-  //     myJson.forEach(function(element) {
-  //       infoArray.push(fetch(`https://api.darksky.net/forecast/a0afd4046e86e2555d0a937cdac811fe/${element["lat"]},${element["lng"]}?exclude==minutely,hourly,daily,alerts,flags`)
-  //       .then(function(response) {
-  //         return response.json();
-  //       })
-  //       .then(function(myJson) {
-  //         // console.log(myJson)
-  //         // return myJson["results"][0]["geometry"]["location"];
-  //       })
-  //       .catch(error => {
-  //         res.setHeader("Content-Type", "application/json");
-  //         res.status(500).send({ error });
-  //       })
-  //       // end of fetch then block
-  //       )
-  //       // push statement paren above
-  //     });
-  //     return infoArray;
-  //
-  //   })
-  //   // next then statement
-  // })
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(JSON.stringify(favoriteForecasts));
+  }
 })
 
 
