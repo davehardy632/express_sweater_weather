49d968a23169f3a188cafa2a4231387de3c69f14
David Hardy <davehardy632@gmail.com>
2019-08-19 07:15:41 -0600

    Refactored helper methods and asynchronous functions.

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -6,6 +6,7 @@ const fetch = require("node-fetch");
 const axios = require('axios');
 const keys = require("dotenv")
 
+
 router.post("/", function(req, res, next) {
   key = req.body.api_key;
   location = req.body.location;
@@ -32,74 +33,8 @@ router.post("/", function(req, res, next) {
   });
 })
 
-////// function return favorite location
-function arrayOfFavoriteLocations(key) {
-  User.findAll({ where: { apiKey: key }})
-  .then(function(user) {
-    id = user[0]["dataValues"]["id"];
-    Location.findAll({ where: { UserId: id }})
-    .then(function(locations) {
-      locationArray = [];
-      for (const key of Object.keys(locations)) {
-        locationArray.push(locations[key]["dataValues"]["name"]);
-      }
-      return locationArray;
-    })
-  })
-  .catch(error => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(500).send({ error });
-  });
-}
-
-
-let fetchDataFromApi = async (location) => {
-  let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GEOCODE_API_KEY}&address=${location}`);
-  let results = await response.json();
-  return results["results"][0]["geometry"]["location"];
-}
-
-let fetchForecastFromApi = async (coordinateObj) => {
-  let response = await fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${coordinateObj["lat"]},${coordinateObj["lng"]}?exclude==minutely,hourly,daily,alerts,flags`);
-  let results = await response.json();
-  return results;
-}
-
-let favoriteLocations = async (key) => {
-  let response = await User.findAll({ where: { apiKey: key }})
-  let userId = response[0]["dataValues"]["id"]
-  let locations = await Location.findAll({ where: { UserId: userId }}).then(returnValue => {
-    return returnValue
-  })
-  .catch(error => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(500).send({ error });
-  });
-  return locations;
-}
-
-let accrueLatLong = async (array) => {
-  let coordinates= [];
-  for (let i = 0; i < array.length; i++) {
-    let res = await fetchDataFromApi(array[i]);
-    coordinates.push(res);
-  }
-  return coordinates;
-}
-
-let accrueForecasts = async (array) => {
-  let forecastArray = [];
-  for (let i = 0; i < array.length; i++) {
-    let res = await fetchForecastFromApi(array[i]);
-    forecastArray.push(res);
-  }
-  return forecastArray;
-}
-
 
 router.get("/", async function(req, res, next) {
-  console.log(process.env.DARK_SKY_API_KEY)
-  console.log(process.env.GEOCODE_API_KEY)
   userKey = req.body.api_key
 
   if (userKey == undefined) {
@@ -108,6 +43,7 @@ router.get("/", async function(req, res, next) {
   } else {
     favorites = await favoriteLocations(userKey)
     favoriteArray = [];
+
     favorites.forEach(function(element) {
       favoriteArray.push(element["dataValues"]["name"]);
     })
@@ -115,21 +51,11 @@ router.get("/", async function(req, res, next) {
     latLongArray = await accrueLatLong(favoriteArray)
     favoriteForecasts = await accrueForecasts(latLongArray)
 
-
     res.setHeader("Content-Type", "application/json");
     res.status(200).send(JSON.stringify(favoriteForecasts));
   }
 })
 
-let authenticateUser = async (apiKey) => {
-  let user = await User.findAll({ where: { apiKey: apiKey }})
-  return user;
-}
-
-let associatedLocation = async (id, locationName) => {
-  let locations = await Location.findAll({ where: {UserId: id, name: locationName}})
-  return locations;
-}
 
 
 router.delete("/", async function(req, res, next) {
@@ -160,5 +86,60 @@ router.delete("/", async function(req, res, next) {
     console.log("Problems")
   }
 })
+
+let authenticateUser = async (apiKey) => {
+  let user = await User.findAll({ where: { apiKey: apiKey }})
+  return user;
+}
+
+let associatedLocation = async (id, locationName) => {
+  let locations = await Location.findAll({ where: {UserId: id, name: locationName}})
+  return locations;
+}
+
+let fetchDataFromApi = async (location) => {
+  let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GEOCODE_API_KEY}&address=${location}`);
+  let results = await response.json();
+  return results["results"][0]["geometry"]["location"];
+}
+
+let fetchForecastFromApi = async (coordinateObj) => {
+  let response = await fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${coordinateObj["lat"]},${coordinateObj["lng"]}?exclude==minutely,hourly,daily,alerts,flags`);
+  let results = await response.json();
+  return results;
+}
+
+let favoriteLocations = async (key) => {
+  let response = await User.findAll({ where: { apiKey: key }})
+  let userId = response[0]["dataValues"]["id"]
+  let locations = await Location.findAll({ where: { UserId: userId }}).then(returnValue => {
+    return returnValue
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({ error });
+  });
+  return locations;
+}
+
+////////// async and helper methods
+
+let accrueLatLong = async (array) => {
+  let coordinates= [];
+  for (let i = 0; i < array.length; i++) {
+    let res = await fetchDataFromApi(array[i]);
+    coordinates.push(res);
+  }
+  return coordinates;
+}
+
+let accrueForecasts = async (array) => {
+  let forecastArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let res = await fetchForecastFromApi(array[i]);
+    forecastArray.push(res);
+  }
+  return forecastArray;
+}
 
 module.exports = router;
